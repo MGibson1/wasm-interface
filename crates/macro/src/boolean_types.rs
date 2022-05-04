3a230139ddf42d5402666acbf53b7f846fd40537
Matt Gibson <mgibson@bitwarden.com>
2022-05-04 10:51:45 -0400

    Add bool option

--- crates/macro/src/boolean_types.rs
+++ crates/macro/src/boolean_types.rs
@@ -1,5 +1,8 @@
 use quote::quote;
 
-pub fn bool_types() -> [String;1] {
-    [quote! { bool }.to_string()]
+pub fn bool_types() -> [String;2] {
+    [
+        quote! { bool }.to_string(),
+        quote! { Option<bool> }.to_string()
+    ]
 }
